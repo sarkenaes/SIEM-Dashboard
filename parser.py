ea6509332bebba82b90d0eab2a66188d48e16255
Saron Nigussie <sarkenaes@gmail.com>
2026-05-29 20:09:36 -0500

    Add source IP

--- parser.py
+++ parser.py
@@ -1,23 +1,35 @@
 import re
 def parse_auth_line(line):
+        
         parts=line.split()
         month =parts[0]
         day =parts[1]
         time = parts[2]
         host = parts[3]
-        message =" ".join(parts[5:])
+        message =" ".join(parts[4:])
         timestamp= f"{month} {day} {time}"
         severity ="low"
         if "Failed password" in message :
-                severity ="high"
-        elif "Invalid user in message":
-            severity= "info"
+            severity ="high"
+        elif "Invalid user" in message:
+            severity= "medium"
+        elif "accepted password" in message:
+            severity ="info"
+        source_ip=None
+        words = message.split()
+        for i, word in enumerate(words):
+            if word == "from" and i+1 < len(words):
+              source_ip = words[i+1]
+              break
+        
         return{
                "timestamp" : timestamp,
                "host": host,
                "message": message,
                "severity": severity,
+               "source_ip": source_ip
         }
-line ="May 28 08:01:12 webserver sshd[1001] : Failed password for root from 198.51.100.5 port 22"
+line ="May 28 08:01:12 webserver sshd[1001]: Failed password for root from 198.51.100.5 port 22"
 result = parse_auth_line(line)
-print(result)
+for key,value in result.items():
+        print(f"{key}: {value}")
